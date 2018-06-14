27bbdd771a7444d7cd97ba68870ae6f5d45b0b29
Oscar Song <oscar2song@gmail.com>
2018-06-13 21:22:16 -0400

    changes for win10

--- utils/mnist_utils.py
+++ utils/mnist_utils.py
@@ -1,18 +1,6 @@
 from tensorflow.examples.tutorials.mnist import input_data
 
-def read_mnist(path='data/MNIST_data'):
-	'''
-	Get MNIST data
-
-	Parameters
-	----------
-	path: str
-		path to downloaded MNIST data
-
-	Return
-	------
-	`obj`:tuple of `obj`:numpy.ndarray
-	'''
+def read_mnist(path='data\\MNIST_data'):
     mnist = input_data.read_data_sets(path, one_hot = True)
     train_X, train_Y, test_X, test_Y = (
     	mnist.train.images, mnist.train.labels, 
