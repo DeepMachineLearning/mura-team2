d3b7b42e9ced6c362db48302d9b275e6846cc95f
madcarrot <8383zhe@gmail.com>
2018-06-23 02:45:33 -0400

    fixed some issue with plots

---
+++ utils/plots.py
@@ -0,0 +1,29 @@
+import itertools
+from sklearn.metrics import confusion_matrix
+from matplotlib import pyplot as plt
+import numpy as np
+
+def plot_confusion_matrix(true_label, pred_label, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues, figsize=None):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    (This function is copied from the scikit docs.)
+    """
+    cm = confusion_matrix(true_label, pred_label)
+    plt.figure(figsize=figsize)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize: cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print(cm)
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j], horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
